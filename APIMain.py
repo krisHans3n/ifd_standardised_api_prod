37e850db8a297d49d72844157d27edce1de26faf
krisHans3n <krishans3n@gmail.com>
2021-01-13 23:26:32 +0000

    Introduced initiale api interface and tested with json carrying list of strings

--- APIMain.py
+++ APIMain.py
@@ -1,20 +1,30 @@
 # import flask
+import sys
+import logging
 from flask import Flask, request, jsonify
 import json
 
 app = Flask(__name__)
 
+logging.basicConfig(level=logging.DEBUG)
 
-@app.route('/imginterface/', methods=['PUT'])
+
+@app.route('/imginterface/', methods=['POST', 'GET'])
 def respond():
-    urls = request.args.get('name')
+    """
+    Test curl:
+    curl -v -H "Content-Type: application/json" -X POST -d '{"urls": ["ver", "sdf", "df"]}' http://127.0.0.1:5000/imginterface/
+    """
+    urls = request.get_json()
+    urls = urls["urls"]
+
     response = {}
 
     if urls is None:
         response["Error"] = "no urls were provided"
     elif urls is not None:
         cnt = len(urls)
-        report = f"you selected {cnt} images"
+        report = f"you selected {cnt} {urls} images"
         response["REPORT"] = report
 
     return jsonify(response)
