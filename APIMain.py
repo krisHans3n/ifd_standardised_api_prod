8dca959e8fdf86aae418e0d9ae35b27ec5911952
krisHans3n <krishans3n@gmail.com>
2021-01-14 21:51:07 +0000

    added docstring

--- APIMain.py
+++ APIMain.py
@@ -3,7 +3,6 @@ import sys
 import logging
 from src import initialise_image_process as iip
 from flask import Flask, request, jsonify
-import json
 
 app = Flask(__name__)
 
@@ -15,17 +14,21 @@ def respond():
     """
     Test curl:
     curl -v -H "Content-Type: application/json" -X POST -d '{"urls": ["https://www.w3schools.com/howto/img_mountains.jpg", "https://www.oxforduniversityimages.com/images/rotate/Image_Spring_17_4.gif"]}' http://127.0.0.1:5000/imginterface/
+
+    TODO:
+    -> clean urls / validate
+    -> add configs
+    -> chunk each url in to return to browser to split payload
+         helps with timeout
     """
     urls = request.get_json()
     urls = urls["urls"]
 
     response = {}
 
-    if urls is None:
+    if urls is None or len(urls) == 0:
         response["Error"] = "no urls were provided"
     elif urls is not None:
-        # cnt = len(urls)
-        # report = f"you selected {cnt} {urls} images"
         report = iip.main_process(urls)
         response["PAYLOAD"] = report
 
